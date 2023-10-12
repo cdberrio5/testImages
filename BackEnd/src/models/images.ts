340b2e7d38ed4f92df3b7dd81cbda9ab53acd635
Cristian Berrio <cristianberrio9864@gmail.com>
2023-10-11 21:53:18 -0500

    Interfaces and image model

---
+++ BackEnd/src/models/images.ts
@@ -0,0 +1,10 @@
+import mongoose, { Schema } from 'mongoose';
+import { Image } from '../interfaces/image';
+
+const ImageSchema: Schema = new Schema({
+  path: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  insertedBy: { type: String, required: true },
+});
+
+export default mongoose.model<Image>('Miner', ImageSchema);
