9ef0f6b4920d4f789b2a1eb6e2155f133269adb9
Cristian Berrio <cristianberrio9864@gmail.com>
2023-10-12 19:06:13 -0500

    new route to see the images

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './components/navbar/navbar';
 import Home from './pages/home/home';
+import Images from './pages/images/images';
 
 import "./App.scss";
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <div className='body'>
         <Routes>
           <Route path="/" Component={Home} />
+          <Route path="images" Component={Images} />
         </Routes>
       </div>
     </Router>
